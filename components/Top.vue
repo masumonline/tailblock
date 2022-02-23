ef6f0b482c52128d2341004c939bffaa3807fe1b
Riaz Uddin Masum <masum@pop-os.localdomain>
2022-02-23 21:54:48 +0600

    first commit

---
+++ components/Top.vue
@@ -0,0 +1,26 @@
+<template>
+<nav class="bg-blue-600 text-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800">
+  <div class="container flex flex-wrap justify-between items-center mx-auto">
+    <a href="#" class="flex">
+        <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Tailwind Block</span>
+    </a>
+    <div class="w-full md:block md:w-auto">
+      <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <li>
+          <a href="#" class="block py-2 pr-4 pl-3 text-white rounded dark:text-white" aria-current="page">Code</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pr-4 pl-3 text-white rounded dark:text-white" aria-current="page">Color</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pr-4 pl-3 text-white rounded dark:text-white" aria-current="page">View</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pr-4 pl-3 text-white rounded dark:text-white" aria-current="page">Mode</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="100hv">
+    <div class="flex">
+      <div class="overflow-y-scroll max-h-screen flex-none w-64">
+        <Aside />
+      </div>
+      
+      <div class="flex-1">
+        <Top />
+        <Nuxt />
+      </div>
+    </div>
+  </div>
+</template>
